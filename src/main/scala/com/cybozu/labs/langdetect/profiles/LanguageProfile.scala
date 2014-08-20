a98e57f3bbc8010ee3eb678fbf31d093f4207a44
wobu <buchnerw@googlemail.com>
2014-08-20 12:40:12 +0200

    removed scala to java collection conversion

--- src/main/scala/com/cybozu/labs/langdetect/profiles/LanguageProfile.scala
+++ src/main/scala/com/cybozu/labs/langdetect/profiles/LanguageProfile.scala
@@ -6,13 +6,15 @@ object LanguageProfile {
   private val languages = Seq("af", "ar", "bg", "bn", "cs", "da", "de", "el", "en", "es", "et", "fa", "fi", "fr", "gu", "he", "hi", "hr", "hu", "id", "it", "ja", "kn", "ko", "lt", "lv", "mk", "ml", "mr", "ne", "nl", "no", "pa", "pl", "pt", "ro", "ru", "sk", "sl", "so", "sq", "sv", "sw", "ta", "te", "th", "tl", "tr", "uk", "ur", "vi", "zh-cn", "zh-tw")
   private val shortTextLanguages = Seq("ar", "bg", "bn", "ca", "cs", "da", "de", "el", "en", "es", "et", "fa", "fi", "fr", "gu", "he", "hi", "hr", "hu", "id", "it", "ja", "ko", "lt", "lv", "mk", "ml", "nl", "no", "pa", "pl", "pt", "ro", "ru", "si", "sq", "sv", "ta", "te", "th", "tl", "tr", "uk", "ur", "vi", "zh-cn", "zh-tw")
 
-  import scala.collection.JavaConversions._
-
   lazy val profiles : java.util.Map[String, String] = {
-    mapAsJavaMap(languages.map(lang => lang -> Source.fromInputStream(LanguageProfile.getClass.getResourceAsStream(lang))(Codec.UTF8).mkString).toMap)
+    val profiles = new java.util.HashMap[String, String]()
+    languages.foreach(lang => profiles.put(lang, Source.fromInputStream(LanguageProfile.getClass.getResourceAsStream(lang))(Codec.UTF8).mkString))
+    profiles
   }
 
   lazy val shortTextProfiles : java.util.Map[String, String] = {
-    mapAsJavaMap(shortTextLanguages.map(lang => lang -> Source.fromInputStream(LanguageProfile.getClass.getResourceAsStream("short/" + lang))(Codec.UTF8).mkString).toMap)
+    val profiles = new java.util.HashMap[String, String]()
+    shortTextLanguages.foreach(lang => profiles.put(lang, Source.fromInputStream(LanguageProfile.getClass.getResourceAsStream("short/" + lang))(Codec.UTF8).mkString))
+    profiles
   }
 }
